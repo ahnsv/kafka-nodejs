7112991543e18e653291b3ce617e1ede21ca7727
humphrey <ahnsv@bc.edu>
2020-09-01 17:05:24 +0900

    create new ingest endpoint

--- index.ts
+++ index.ts
@@ -1,13 +1,30 @@
-import express from 'express';
+import express, {} from 'express';
 import {KafkaManager} from "./main";
+import {ProduceRequest} from "kafka-node";
 
 const app = express();
+app.use(express.json())
+
+const kafkaMgr = new KafkaManager();
+kafkaMgr.initialize();
 
 app.get('/', (req: express.Request, res: express.Response) => {
-    const kafkaMgr = new KafkaManager();
-    kafkaMgr.initialize();
     res.send('Hello World!');
 });
+
+app.post('/ingest', (req: express.Request, res: express.Response) => {
+    try {
+        const body = req.body;
+        const payload: ProduceRequest = {
+            topic: 'topic1',
+            messages: body
+        };
+        kafkaMgr.sendMessage(payload);
+        res.send('OK')
+    } catch (e) {
+        console.log(e)
+    }
+})
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import {Consumer, KafkaClient, KeyedMessage, Producer} from 'kafka-node'
+import {Consumer, KafkaClient, KeyedMessage, Producer, ProduceRequest} from 'kafka-node'
 
 
 const km = new KeyedMessage('key', 'message')
@@ -12,7 +12,7 @@ export class KafkaManager {
     private client: KafkaClient;
     private producer: Producer;
     // TODO: implement with kue or rxjs
-    private static jobQueue: Array<object> = []
+    private jobQueue: Array<ProduceRequest> = []
     private consumer: Consumer;
 
 
@@ -20,7 +20,7 @@ export class KafkaManager {
         this.client = new KafkaClient({kafkaHost: 'localhost:9092'});
         this.producer = new Producer(this.client);
         this.consumer = new Consumer(this.client, [
-            { topic: 'topic1', partition: 0 }, { topic: 'topic2', partition: 0 }
+            {topic: 'topic1', partition: 0}, {topic: 'topic2', partition: 0}
         ], {});
     }
 
@@ -32,9 +32,13 @@ export class KafkaManager {
     private initializeProducer() {
         this.producer.on('ready', () => {
             setInterval(() => {
-                    this.producer.send(payloads, function (err, data) {
+                    if (this.jobQueue.length === 0) {
+                        return;
+                    }
+                    this.producer.send(this.jobQueue, function (err, data) {
                         console.log(`[producer] - sent ${JSON.stringify(data)}`);
-                    })
+                    });
+                    this.jobQueue = []
                 }, 1000
             )
         });
@@ -50,7 +54,7 @@ export class KafkaManager {
         });
     }
 
-    static sendMessage(event: object) {
+    sendMessage(event: ProduceRequest) {
         this.jobQueue.push(event);
     }
 }
